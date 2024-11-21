ba2a03619135071e24f5114c3baa8e067182b8a1
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-21 09:15:14 +0100

    add files

---
+++ Kata-AnnalynsInfiltration-Java-dev/src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -0,0 +1,21 @@
+package org.factoriaf5.infiltration;
+
+public class AnnalynsInfiltration {
+
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,boolean petDogIsPresent) {
+      return (prisonerIsAwake && !knightIsAwake && !archerIsAwake) || (!archerIsAwake && petDogIsPresent);  
+    }
+
+}
